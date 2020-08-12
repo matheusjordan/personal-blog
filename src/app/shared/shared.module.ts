70179f815d68310356e5e01fd0b41ad3a226f741
matheus jordan <matheusj1599@gmail.com>
2020-08-11 23:45:02 -0300

    Created nav buttons layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AvatarContainerComponent } from './components/avatar-container/avatar-container.component';
 import { IonicModule } from '@ionic/angular';
+import { ButtonsContainerComponent } from './components/buttons-container/buttons-container.component';
 
 
 
 @NgModule({
-  declarations: [AvatarContainerComponent],
+  declarations: [
+    AvatarContainerComponent,
+    ButtonsContainerComponent
+  ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    AvatarContainerComponent
+    AvatarContainerComponent,
+    ButtonsContainerComponent
   ]
 })
 export class SharedModule { }
