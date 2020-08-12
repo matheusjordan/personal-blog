d8e85c6ea334bb3e6fae97c800709406453da2a6
matheus jordan <matheusj1599@gmail.com>
2020-08-12 01:43:45 -0300

    Added data bindings

--- src/app/shared/components/avatar-container/avatar-container.component.ts
+++ src/app/shared/components/avatar-container/avatar-container.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avatar-container.component.scss']
 })
 export class AvatarContainerComponent implements OnInit {
+  avatarImage = 'https://scontent.fjdo10-1.fna.fbcdn.net/v/t1.0-9/110042783_650417739155087_4770279522928906262_n.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=mjf1ET1ZtH4AX8XpQQy&_nc_ht=scontent.fjdo10-1.fna&oh=bfaf72ea13bcf0d7abab0b517c185b3e&oe=5F5A29B4';
+  username = 'Matheus Jordan';
+  titlename = '👨🏾‍💻 Software Developer';
 
   constructor() { }
 
