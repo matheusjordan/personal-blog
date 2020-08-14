cf91c7c76ce785a7553020a6d5975f85be902c59
matheus jordan <matheusj1599@gmail.com>
2020-08-14 15:54:09 -0300

    Added buttons resposivity and navigate to LoginPage

--- src/app/shared/components/buttons-container/buttons-container.component.ts
+++ src/app/shared/components/buttons-container/buttons-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-buttons-container',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  toLogin(): void {
+    this.router.navigate(['/login']);
   }
 
 }
